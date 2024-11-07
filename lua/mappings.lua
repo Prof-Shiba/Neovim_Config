5647425d019bfa60ac951c99f8733e27b6056b15
DTM_UA <166961473+ProfShibe@users.noreply.github.com>
2024-11-07 14:54:29 -0600

    Update mappings.lua
    Better executable naming for C++ compilation and add theme switcher comment

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,21 +5,21 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- CTRL + T : Fancier Theme Switcher
 map("n", "<C-t>", function()
   require("nvchad.themes").open {border = true}
 end, {})
 
 -- C++ below
 -- F5 to compile all C++ files in the current directory
-map("n", "<F5>", ":!g++ *.cpp -o program && echo 'Compiled successfully!'<CR>", { noremap = true, silent = true })
+map("n", "<F5>", ":!g++ *.cpp -o main && echo 'Compiled successfully!'<CR>", { noremap = true, silent = true })
 
 -- F6 to run the compiled program
-map("n", "<F6>", ":!./program<CR>", { noremap = true, silent = true })
+map("n", "<F6>", ":!./main<CR>", { noremap = true, silent = true })
 
 -- F7 to compile and run all C++ files
-map("n", "<F7>", ":!g++ *.cpp -o program && ./program<CR>", { noremap = true, silent = true })
+map("n", "<F7>", ":!g++ *.cpp -o main && ./main<CR>", { noremap = true, silent = true })
 
 -- F8 to compile with debug symbols
-map("n", "<F8>", ":!g++ -g -O0 *.cpp -o program", { noremap = true, silent = true })
+map("n", "<F8>", ":!g++ -g -O0 *.cpp -o main", { noremap = true, silent = true })
 -- End of C++
