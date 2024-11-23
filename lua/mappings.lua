7b7235106dfe81ec73bdf93ee5449ecc8485b9d6
ProfShibe <166961473+ProfShibe@users.noreply.github.com>
2024-11-22 20:25:34 -0600

    Add border effect to volt menu plugin. Lazy updated plugins

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -17,7 +17,7 @@ vim.keymap.set("n", "<RightMouse>", function()
   vim.cmd.exec '"normal! \\<RightMouse>"'
 
   local options = vim.bo.ft == "NvimTree" and "nvimtree" or "default"
-  require("menu").open(options, { mouse = true })
+  require("menu").open(options, { mouse = true, border = true})
 end, {})
 
 -- C++ below
