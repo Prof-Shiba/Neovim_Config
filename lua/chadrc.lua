5e6633f447818384d6525d1abe33d2db67233fb7
ProfShibe <166961473+ProfShibe@users.noreply.github.com>
2025-01-18 23:08:17 -0600

    Update plugins and theme

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -6,9 +6,9 @@
 local M = {}
 
 M.base46 = {
-	theme = "catppuccin",
+	theme = "gruvbox",
   transparency = false,
-  theme_toggle = {"catppuccin", "one_light"},
+  theme_toggle = {"gruvbox", "one_light"},
   -- hl_override = require "configs.highlights".override, -- overrides the default white color
 }
 
