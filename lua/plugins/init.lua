27f108931e9d64d9b406ec743a05dcc238cde0c5
chilicheesedawg <166961473+chilicheesedawg@users.noreply.github.com>
2025-03-23 17:15:33 -0500

    add silicon plugin to screenshot code easily

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -34,6 +34,45 @@ return {
   },
 
   {
+    "michaelrommel/nvim-silicon",
+    lazy = true,
+    cmd = "Silicon",
+    init = function()
+      local wk = require("which-key")
+      wk.add({
+          mode = { "v" },
+          { "<leader>s",  group = "Silicon" },
+          { "<leader>sc", function() require("nvim-silicon").clip() end, desc = "Copy code screenshot to clipboard" },
+          { "<leader>sf", function() require("nvim-silicon").file() end,  desc = "Save code screenshot as file" },
+          { "<leader>ss", function() require("nvim-silicon").shoot() end,  desc = "Create code screenshot" },
+      })
+    end,
+
+    main = "nvim-silicon",
+    opts = {
+      font = "JetBrainsMono Nerd Font=20;Noto Color Emoji=20",
+
+      window_title = function()
+        return vim.fn.fnamemodify(
+            vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()),
+            ":t"
+	    )
+      end,
+
+      no_line_number = false,
+      theme = "OneHalfDark",
+      to_clipboard = true,
+
+      language = function()
+        return vim.fn.fnamemodify(
+          vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()),
+          ":e"
+        )
+      end,
+    },
+  },
+
+  {
     "windwp/nvim-ts-autotag",
     dependencies = "nvim-treesitter/nvim-treesitter",
     config = function()
