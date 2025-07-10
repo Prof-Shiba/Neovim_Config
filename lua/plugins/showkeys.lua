26dec44c80e3a1d25ea4d6f5be9be07ec58fa8d4
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-07-10 11:36:35 -0500

    update: astro files

---
+++ lua/plugins/showkeys.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "nvzone/showkeys",
+    opts = {
+      timeout = 3,
+      maxkeys = 4,
+      show_count = true,
+    }
+  },
+}
---
+++ lua/plugins/silicon.lua
@@ -0,0 +1,41 @@
+return {
+{
+    "michaelrommel/nvim-silicon",
+    lazy = true,
+    cmd = "Silicon",
+    init = function()
+      local wk = require("which-key")
+      wk.add({
+          mode = { "v" },
+          { "<leader>s",  group = "Silicon" },
+          { "<leader>sc", function() require("nvim-silicon").clip() end, desc = "Copy code screenshot to clipboard" },
+          { "<leader>sf", function() require("nvim-silicon").file() end,  desc = "Save code screenshot as file" },
+          { "<leader>ss", function() require("nvim-silicon").shoot() end,  desc = "Create code screenshot" },
+      })
+    end,
+
+    main = "nvim-silicon",
+    opts = {
+      font = "JetBrainsMono Nerd Font=20;Noto Color Emoji=20",
+
+      window_title = function()
+        return vim.fn.fnamemodify(
+            vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()),
+            ":t"
+	    )
+      end,
+
+      no_line_number = false,
+      theme = "gruvbox-dark",
+      background_image = "/home/shiba/Pictures/wallpapers/misty_forest.jpg",
+      to_clipboard = true,
+
+      language = function()
+        return vim.fn.fnamemodify(
+          vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()),
+          ":e"
+        )
+      end,
+    },
+  },
+}
