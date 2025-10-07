d17da9220d111baaf4755247f56a2b114c333417
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-10-07 11:10:01 -0500

    fix: LSP Config, update plugins

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,17 +1,18 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
+-- local lspconfig = require "lspconfig"
 
 -- ts_ls is typescript + javascript 
 local servers = { "html", "cssls", "pyright", "lua_ls", "clangd", "ts_ls", "bashls", "rust_analyzer", "neocmake"}
 local nvlsp = require "nvchad.configs.lspconfig"
 
--- lsps with default config
+vim.lsp.enable(servers)
+
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
+  vim.lsp.config(lsp, {
     on_attach = nvlsp.on_attach,
     on_init = nvlsp.on_init,
     capabilities = nvlsp.capabilities,
-  }
+  })
 end
