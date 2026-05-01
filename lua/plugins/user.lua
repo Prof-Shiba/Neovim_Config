1604a61ad9db3d2da8c0f1552215c8fcfbd6c9fa
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2026-05-01 06:22:24 -0500

    change: disable nvim tokyonight by default

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -19,17 +19,15 @@ return {
     opts = {},
   },
 
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    dependencies = { "hyde.nvim" },
-    config = function()
-      require("hyde").setup_tokyonight()
-    end,
-  },
-
-  -- == Examples of Overriding Plugins ==
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   dependencies = { "hyde.nvim" },
+  --   config = function()
+  --     require("hyde").setup_tokyonight()
+  --   end,
+  -- },
 
   -- customize dashboard options
   {
